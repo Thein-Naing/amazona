80956bfbcfc506ef94d695726be5b2b20ba77b8b
Thein Naing <theinnaing0908@gmail.com>
2023-07-08 11:03:18 +0800

    create Home.jsx in screen folder/src to render products  UI  and also create Home.css in Style folder and import to App.js and put inside Header component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Login from './screens/Login';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <Routes>
-          <Route path= '/login' element={<><Login /></>}/>
-          <Route path= '/' element={<><Header /></>}/>
+          <Route
+            path="/login"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
-      </Router>
-    <h1>amazona</h1>
-    </div>
+      </div>
+    </Router>
   );
 }
 
