a55d81b9838fe6448593875bef460bbd99cfe65b
Thein Naing <theinnaing0908@gmail.com>
2023-07-08 14:47:24 +0800

    create Checkout.jsx  in screen folder/src & Checkout.css in Style/src

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
+import Checkout from "./screens/Checkout";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
           <Route
             path="/login"
             element={
---
+++ src/screens/Checkout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Style/Checkout.css';
+
+
+
+const Checkout = () => {
+  return (
+    <div className='checkout'>
+      <div className="checkout_left">
+        <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+        alt=""
+        className="checkout_ad" />
+      </div>
+    </div>
+  )
+}
+
+export default Checkout
